844700bad3c48669f7d56e65ecda00b0d0d2e8fb
anilakv123 <anilamurali0@gmail.com>
2022-09-15 13:12:02 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,14 @@
+//to view content inside result whenever we click on the button
+function displayNum(num){
+    console.log(result);
+    result.value += num
+}
+function allClear(){
+    result.value = ""
+}
+function evalExp(){
+    result.value = eval(result.vlaue)
+}
+function lastDigit(){
+    result.value= result.value.slice(0,-1)
+}
